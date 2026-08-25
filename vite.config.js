f31122907bff363705a9aab14795eeede41a01f7
fatedevv <m.sungurov19@gmail.com>
2026-08-25 17:29:24 +0500

    fix1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  base: '/glslgalaxy/',
-  plugins: [glsl()]
+  plugins: [glsl({
+    compress: false 
+  })]
 });
